3693bbead59a6db2f9a6cd08ff3bb38c983b36e8
Roger Coverzola <diolink.4103@gmail.com>
2020-08-27 17:13:23 -0300

    DummyData.js append new data, because will using in Post.js

--- src/assets/DummyData.js
+++ src/assets/DummyData.js
@@ -171,4 +171,31 @@ const dataAbout = [
   { picUrl: '/mailbox.jpg', name: 'Content Creation' },
 ];
 
-module.exports = { data, dataAbout };
+const post = [
+  {
+    picUrl: '/mailbox.jpg',
+    title: 'Conterence Room Setup Or Upgrade',
+    description:
+      'Nisi labore id aliquip elit culpa. Ad nulla id fugiat nulla in fugiat quis sit. Dolor sit commodo laborum magna. Cillum ut incididunt in velit nostrud Esse ad anim ipsum culpa do in in velit sunt qui eiusmod sunt ad reprehenderit. Commodo irure cupidatat consequat mollit fugiat duis veniam esse aute eu sint Lorem dolore. Duis nisi enim nulla quis aliquip id duis.In tempor esse irure et et quis. Commodo excepteur eiusmod nulla eiusmod velit exercitation veniam exercitation ut nostrud laborum eiusmod. Fugiat nulla veniam nostrud amet quis pariatur excepteur sit anim excepteur velit adipisicing esse. Ad nulla ut tempor velit incididunt nisi dolor aute pariatur do mollit. Cupidatat excepteur adipisicing consectetur eu sint anim veniam id non. Officia tempor aliqua proident duis voluptate. Quis veniam magna commodo exercitation quis.',
+  },
+  {
+    picUrl: '/mailbox.jpg',
+    title: 'VoIP And Unfied Communicarions Support',
+    description:
+      'Nisi labore id aliquip elit culpa. Ad nulla id fugiat nulla in fugiat quis sit. Dolor sit commodo laborum magna. Cillum ut incididunt in velit nostrud.Nisi consequat consequat velit anim. In commodo proident irure consequat aute dolore in ut deserunt minim laborum. Mollit est magna duis esse enim occaecat. Sint ullamco velit culpa exercitation mollit commodo. Irure eiusmod ullamco nisi ad. Proident dolor et reprehenderit commodo esse est commodo sit pariatur ut.Pariatur culpa dolor esse mollit consequat id ipsum ad. Esse est anim nostrud duis adipisicing dolor nisi tempor eu commodo voluptate ea cupidatat. Amet laboris reprehenderit laborum dolor in. Ut deserunt veniam veniam veniam esse nisi in culpa ut tempor. Fugiat eu pariatur nisi consectetur commodo. Exercitation voluptate reprehenderit elit anim ex sunt quis magna. Et non elit do in incididunt officia proident irure ut.',
+  },
+  {
+    picUrl: '/mailbox.jpg',
+    title: 'Ads Creation & Solutions',
+    description:
+      'Nisi labore id aliquip elit culpa. Ad nulla id fugiat nulla in fugiat quis sit. Dolor sit commodo laborum magna. Cillum ut incididunt in velit nostrud.Consectetur officia ullamco minim dolor in minim Lorem. Nulla nostrud exercitation in ex eu est sit aliqua nisi ea laboris aute. Cupidatat est enim adipisicing do id duis. Quis quis anim elit ex. Officia anim ex cillum mollit. Adipisicing ad reprehenderit eiusmod adipisicing.Excepteur proident cillum ea veniam eu proident occaecat ullamco enim magna ut dolor est. Deserunt laborum anim dolore anim sunt fugiat amet pariatur. Sunt enim anim non reprehenderit aliquip ex eiusmod velit ut tempor mollit anim mollit.',
+  },
+  {
+    picUrl: '/mailbox.jpg',
+    title: 'iOS And Android Development',
+    description:
+      'Nisi labore id aliquip elit culpa. Ad nulla id fugiat nulla in fugiat quis sit. Dolor sit commodo laborum magna. Cillum ut incididunt in velit nostrud.Esse do non aute do. Et aliquip minim sit do dolor voluptate non do excepteur nisi. Incididunt aliqua laboris ea ut minim culpa sunt Lorem esse cillum nisi.Non commodo laboris id amet amet ad mollit consequat. Lorem magna aliquip non in dolore est voluptate cillum quis do et. Cillum qui non non ipsum ea. Veniam ea deserunt ad et adipisicing consequat proident aliquip ad cupidatat.',
+  },
+];
+
+module.exports = { data, dataAbout, post };
