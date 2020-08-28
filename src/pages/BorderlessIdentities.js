d95a65a9ef07c6f192c3c9d1dfbbfaa91ff633af
Roger Coverzola <diolink.4103@gmail.com>
2020-08-28 19:33:21 -0300

    Presentation of the Layout

--- src/pages/BorderlessIdentities.js
+++ src/pages/BorderlessIdentities.js
@@ -2,23 +2,33 @@ import React from 'react';
 import Container from '../components/Container';
 import BackgroundImage from '../assets/Background.png';
 import BackgroundBanner from '../assets/BackgroundBanner.png';
+import woman from '../assets/woman.jpg';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+import { Grid, TextareaAutosize } from '@material-ui/core';
 
 const styles = makeStyles({
   root: {},
   backgroundGlobal: {
     background: '#f2ff',
-    height: '100vh',
+    height: '100%',
   },
   backgroundBaner: {
-    flex: 1,
     background: `url(${BackgroundBanner})`,
     backgroundSize: 'cover',
-    width: '60vw',
-    height: 250,
+    height: 350,
   },
+  containerItems: { padding: 15 },
   titleItems: { fontSize: '18px' },
+  titleItem: { margin: '0 15px' },
+  justifyText: { textAlign: 'right' },
+  picItems: {
+    margin: '0 18px',
+    width: 300,
+    height: 300,
+  },
+  itemPar: {
+    flexDirection: 'row-reverse',
+  },
 });
 
 const BorderlessIdentities = () => {
@@ -29,14 +39,47 @@ const BorderlessIdentities = () => {
       transparent={'none'}
     >
       <div className={classes.root}>
-        <Grid item className={classes.backgroundGlobal}>
+        <Grid item md={12} className={classes.backgroundGlobal}>
           <Grid item md={12}>
             <div className={classes.backgroundBaner}></div>
           </Grid>
-          <Grid container md={12} justify="center">
+          <Grid container justify="center" md={12}>
             <h1 className={classes.titleItems}>
               What is Borderless Identities?
             </h1>
+          </Grid>
+          <Grid item md={12}>
+            <Grid container md={12} alignItems="flex-start">
+              <Grid item md={4}>
+                <img src={woman} className={classes.picItems} />
+              </Grid>
+              <Grid item md={4}>
+                <h3 className={classes.titleItem}>
+                  Labore id ea veniam esse nostrud incididunt elit
+                  irure consectetur velit minim voluptate cillum
+                  officia.
+                </h3>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              md={12}
+              alignItems="flex-start"
+              className={classes.itemPar}
+            >
+              <Grid container md={4}>
+                <img src={woman} className={classes.picItems} />
+              </Grid>
+              <Grid item md={4}>
+                <h3
+                  className={`${classes.titleItem} ${classes.justifyText} `}
+                >
+                  Labore id ea veniam esse nostrud incididunt elit
+                  irure consectetur velit minim voluptate cillum
+                  officia.
+                </h3>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </div>
