51a8eed31c73ba25d316a26fcf9b7de072343575
Roger Coverzola <diolink.4103@gmail.com>
2020-08-31 18:57:25 -0300

    An addition was made to the Borderless Identities.js page for the implementation of a component.

--- src/pages/BorderlessIdentities.js
+++ src/pages/BorderlessIdentities.js
@@ -2,33 +2,24 @@ import React from 'react';
 import Container from '../components/Container';
 import BackgroundImage from '../assets/Background.png';
 import BackgroundBanner from '../assets/BackgroundBanner.png';
-import woman from '../assets/woman.jpg';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, TextareaAutosize } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { borderlessItem as DummyItems } from '../assets/DummyData';
+import BorderlessItems from '../components/BorderlessItems';
 
 const styles = makeStyles({
   root: {},
   backgroundGlobal: {
-    background: '#f2ff',
+    background: '#fff',
     height: '100%',
   },
   backgroundBaner: {
     background: `url(${BackgroundBanner})`,
     backgroundSize: 'cover',
-    height: 350,
+    backgroundPosition: 'center',
+    paddingBottom: '30%',
   },
-  containerItems: { padding: 15 },
   titleItems: { fontSize: '18px' },
-  titleItem: { margin: '0 15px' },
-  justifyText: { textAlign: 'right' },
-  picItems: {
-    margin: '0 18px',
-    width: 300,
-    height: 300,
-  },
-  itemPar: {
-    flexDirection: 'row-reverse',
-  },
 });
 
 const BorderlessIdentities = () => {
@@ -48,38 +39,15 @@ const BorderlessIdentities = () => {
               What is Borderless Identities?
             </h1>
           </Grid>
-          <Grid item md={12}>
-            <Grid container md={12} alignItems="flex-start">
-              <Grid item md={4}>
-                <img src={woman} className={classes.picItems} />
-              </Grid>
-              <Grid item md={4}>
-                <h3 className={classes.titleItem}>
-                  Labore id ea veniam esse nostrud incididunt elit
-                  irure consectetur velit minim voluptate cillum
-                  officia.
-                </h3>
-              </Grid>
-            </Grid>
-            <Grid
-              container
-              md={12}
-              alignItems="flex-start"
-              className={classes.itemPar}
-            >
-              <Grid container md={4}>
-                <img src={woman} className={classes.picItems} />
-              </Grid>
-              <Grid item md={4}>
-                <h3
-                  className={`${classes.titleItem} ${classes.justifyText} `}
-                >
-                  Labore id ea veniam esse nostrud incididunt elit
-                  irure consectetur velit minim voluptate cillum
-                  officia.
-                </h3>
-              </Grid>
-            </Grid>
+          <Grid item>
+            {DummyItems.map((item, i) => (
+              <BorderlessItems
+                background={`url(${item.picUrl})`}
+                description={item.description}
+                textAlign={i % 2 ? '' : 'left'}
+                flexDirection={i % 2 ? '' : 'row'}
+              />
+            ))}
           </Grid>
         </Grid>
       </div>
