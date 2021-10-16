aa8048ac9105023928bd2c95e4a0e110e7cb022d
Roger BC <diolink.4103@gmail.com>
2021-10-16 00:34:08 -0300

    add images in page

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -3,22 +3,99 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import Polygon1 from '../assets/Polygon1.png';
+import Polygon2 from '../assets/Polygon2.png';
+import Polygon3 from '../assets/Polygon3.png';
+
 const UnderConstruction = ({ imgUrl }) => {
   const classes = useStyles({
     imgUrl,
   });
-  return <Grid item xs={12} className={classes.backgroundImg} />;
+  return (
+    <Grid
+      container
+      justify="space-between"
+      alignItems="center"
+      className={classes.backgroundRoot}
+    >
+      <Grid item xs={3}></Grid>
+
+      <Grid
+        item
+        xs={7}
+        container
+        justify="center"
+        className={classes.boxImg}
+      >
+        <Grid item xs={8} className={classes.imgContainer}>
+          <div className={classes.backgroundImg1} />
+          <div className={classes.backgroundImg} />
+        </Grid>
+        <div className={classes.backgroundImg2} />
+        <div className={classes.backgroundImg3} />
+      </Grid>
+    </Grid>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
-  backgroundImg: {
-    width: '100%',
+  backgroundRoot: {
+    marginTop: theme.spacing(5),
+    backgroundColor: 'rgba(187, 109, 199, 0.13)',
+    background: 'transparent',
+    borderRadius: 15,
+  },
+  boxImg: { position: 'relative', height: '100vh' },
+  imgContainer: {
+    zIndex: 3,
+    position: 'relative',
+    width: '100vw',
     height: '100%',
+  },
+  backgroundImg: {
     backgroundImage: ({ imgUrl }) => `url(${imgUrl})`,
-    backgroundSize: 'contain',
+    backgroundSize: '85%',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
-    paddingBottom: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: -80,
+    right: 0,
+  },
+  backgroundImg1: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundImage: `url(${Polygon1})`,
+    backgroundSize: '98%',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  },
+  backgroundImg2: {
+    zIndex: 1,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: `url(${Polygon2})`,
+    backgroundSize: '60%',
+    backgroundPosition: 'right',
+    backgroundRepeat: 'no-repeat',
+  },
+  backgroundImg3: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: `url(${Polygon3})`,
+    backgroundSize: '30%',
+    backgroundPosition: 'right',
+    backgroundRepeat: 'no-repeat',
   },
 }));
 
--- src/pages/underConstruction.js
+++ src/pages/underConstruction.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Container from '../components/Container';
 import UnderConstruction from '../components/UnderConstruction';
+import BackgroundImage from '../assets/Trama.png';
 
 const underConstructionPage = () => {
+  const classes = useStyles();
   const {
     allContentfulAsset: {
       edges: [
@@ -34,10 +37,23 @@ const underConstructionPage = () => {
   `);
 
   return (
-    <Container>
+    <Container
+      background={`url(${BackgroundImage})`}
+      innerBackground={'none'}
+      className={classes.root}
+    >
       <UnderConstruction imgUrl={url} />
     </Container>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: '25px 34px 50px',
+    '@media (min-width: 1362px)': {
+      maxWidth: '90%',
+    },
+  },
+}));
 
 export default underConstructionPage;
