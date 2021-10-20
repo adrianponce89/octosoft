49735226096cee4fc286b7e3a7ff67efbf1f3786
Roger BC <diolink.4103@gmail.com>
2021-10-20 12:18:05 -0300

    implement Mailchimp in UnderConstrunction

--- src/components/UnderConstruction/index.js
+++ src/components/UnderConstruction/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import addToMailchimp from 'gatsby-plugin-mailchimp';
+
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -9,6 +11,22 @@ import Left from './Left';
 const UnderConstruction = ({ imgUrl }) => {
   const classes = useStyles();
   const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addToMailchimp(email)
+      .then(({ msg, result }) => {
+        if (result !== 'success') {
+          throw msg;
+        }
+        alert(msg);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+    setEmail('');
+  };
+
   return (
     <Grid
       container
@@ -16,7 +34,11 @@ const UnderConstruction = ({ imgUrl }) => {
       alignItems="center"
       className={classes.root}
     >
-      <Left email={email} setEmail={setEmail} />
+      <Left
+        email={email}
+        setEmail={setEmail}
+        handleSubmit={handleSubmit}
+      />
       <Image imgUrl={imgUrl} />
     </Grid>
   );
