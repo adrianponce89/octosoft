fc5f63a12c868744e00e1ffb9b279cc21437735f
Roger BC <diolink.4103@gmail.com>
2021-10-22 13:45:44 -0300

    404 page is created

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from '../components/Container';
+import NotFound from '../components/NotFound';
+
+const NotFoundPage = () => {
+  return (
+    <Container>
+      <NotFound />
+    </Container>
+  );
+};
+
+export default NotFoundPage;
