7cda76f89ef0b0cf021babb247b22e7e80e3b10c
Agustina Ruidiaz <agusruidiaz@hotmail.com>
2021-10-24 07:56:38 -0300

    export zip function

--- src/components/Brand/IconsBrand/buttonAll.js
+++ src/components/Brand/IconsBrand/buttonAll.js
@@ -3,51 +3,61 @@ import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
+import {filesZip} from '../../../utils/logosBase64'
 
-const ButtonAll = ({files}) => {
+const ButtonAll = () => {
+  // const [files, setFiles] = useState()
   const classes = useStyles();
 
-  const toDataURL = (url) =>
-  fetch(url)
-    .then((response) => response.blob())
-    .then(
-      (blob) =>
-        new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.onloadend = () => resolve(reader.result);
-          reader.onerror = reject;
-          reader.readAsDataURL(blob);
-        }),
-    );
+  // const convertToBase64 = (filesToconvert) =>{
+  //   let arrayFiles = [];
+  //   Array.from(filesToconvert).forEach(file =>{
+  //     let reader = new FileReader();
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => {
+  //       let array = [];
+  //       let base64 = reader.result
+  //       array = base64.split(',')
+  //       arrayFiles.push({base64: array[1], name: file.name})
+  //       console.log(array[1])
+  //     };
+  //   })
+  //   setFiles({...files, arrayFiles})
+  // }
+
+  // const toDataURL = (url) =>
+  // fetch(url)
+  //   .then((response) => response.blob())
+  //   .then(
+  //     (blob) =>
+  //       new Promise((resolve, reject) => {
+  //         const reader = new FileReader();
+  //         reader.onloadend = () => resolve(reader.result);
+  //         reader.onerror = reject;
+  //         reader.readAsDataURL(blob);
+  //       }),
+  //   );
   
 
   const onDownloadZip = async () => {
     let zip = new JSZip();
-    let img = zip.folder('logos');
-
-    // files.map( async (file,index) => {
-    //   const dataUrl = await toDataURL(file)
-    //   console.log('mapping', dataUrl);
-    //   let array = [];
-    //   array = dataUrl.split(',');
-    //   img.file(`octoLogo${index}`, array[1], { base64: true });
-    // })
-
-    const dataUrl = await toDataURL(files[0]);
-    console.log('mapping', dataUrl);
-    let array = [];
-    array = dataUrl.split(',');
-    img.file(`octoLogo`, array[1], { base64: true });
-
+    const { arrayFiles } = filesZip;
+    let folder = zip.folder('logos');
+    arrayFiles.map(file => {
+      folder.file(file.name, file.base64, { base64: true });
+    })
     zip.generateAsync({ type: 'blob' }).then(function (content) {
       saveAs(content, 'logosOctosoft.zip');
     });
   };
   
   return (
+    <>
     <Button className={classes.button} onClick={() => onDownloadZip()}>
-      DOWNLOAD ALL LOGOS
+     DOWNLOAD ALL LOGOS
     </Button>
+    {/* <input type="file" multiple onChange={(e) =>{convertToBase64(e.target.files)}}/> */}
+    </>
   );
 };
 const useStyles = makeStyles((theme) => ({
