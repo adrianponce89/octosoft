5980b7bd92eb3b0e4eb21205fce8cd6883089c8c
Diego <diego.nicolas.gandara@gmail.com>
2021-10-26 16:39:54 -0300

    Added some border radious to material ui input classes. Also, at Contentful, we -Andres and I- could change the color that was asked by Mer -Andres was having problems with server-.

--- src/components/PrimaryInput.js
+++ src/components/PrimaryInput.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { FormControl, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    borderRadius: '10rem',
+    '& .MuiInputBase-input': {
+      borderRadius: '10rem',
+    },
+    '& .MuiOutlinedInput-input': {
+      borderRadius: '10rem',
+    },
+    '& .PrivateNotchedOutline-root-84': {
+      borderRadius: '10rem',
+    },
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderRadius: '10rem',
+    },
+  },
+}));
 
 const PrimaryInput = ({ className, variant, ...otherProps }) => {
+  const classes = useStyles();
   return (
     <FormControl variant={variant} size="small" fullWidth>
-      <TextField variant={variant} {...otherProps} />
+      <TextField
+        className={`${classes.root} ${className}`}
+        variant={variant}
+        {...otherProps}
+      />
     </FormControl>
   );
 };
