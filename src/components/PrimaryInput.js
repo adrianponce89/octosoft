85da0525b7bacd18ae932febf18a82552270c98e
Diego <diego.nicolas.gandara@gmail.com>
2021-10-27 18:45:32 -0300

    Correction: the border-radious was no for the input boxes but for the dropdown menu at /Navbar/Contact. Also changed the text from 'Our services' to 'Our Services'.

--- src/components/PrimaryInput.js
+++ src/components/PrimaryInput.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import { FormControl, TextField } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    borderRadius: '10rem',
-    '& .MuiInputBase-input': {
-      borderRadius: '10rem',
-    },
-    '& .MuiOutlinedInput-input': {
-      borderRadius: '10rem',
-    },
-    '& .PrivateNotchedOutline-root-84': {
-      borderRadius: '10rem',
-    },
-    '& .MuiOutlinedInput-notchedOutline': {
-      borderRadius: '10rem',
-    },
-  },
-}));
 
 const PrimaryInput = ({ className, variant, ...otherProps }) => {
-  const classes = useStyles();
   return (
     <FormControl variant={variant} size="small" fullWidth>
       <TextField
-        className={`${classes.root} ${className}`}
+        className={`${className}`}
         variant={variant}
         {...otherProps}
       />
