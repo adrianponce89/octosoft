b33823820c78652c2a79550d2bf11160f8fac342
Roger BC <diolink.4103@gmail.com>
2021-12-06 17:56:46 -0300

    The subcategory card is created

--- src/components/Services/SubCategories.js
+++ src/components/Services/SubCategories.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
+
 import Titles from '../Titles';
+
+import CardSubCategories from './CardSubCategories';
+
+const SubCategories = () => {
+  const classes = useStyles();
+  const cards = [1, 2, 3, 4, 5, 6];
+  return (
+    <Grid className={classes.root}>
+      <Titles title="Software Engineering" color="#8249CD" />
+      <Grid
+        container
+        justify="center"
+        className={classes.cardContainer}
+      >
+        {cards.map((item, i) => (
+          <CardSubCategories key={`${item + i}`} />
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
+
 const useStyles = makeStyles((theme) => ({
   root: {
     paddingTop: theme.spacing(20),
   },
+  cardContainer: {
+    paddingLeft: theme.spacing(10),
+    paddingRight: theme.spacing(10),
+  },
 }));
-const SubCategories = () => {
-  const classes = useStyles();
-  return (
-    <Grid className={classes.root}>
-      <Titles title="Software Engineering" color="#8249CD" />
-      <h1>SubCategories</h1>
-    </Grid>
-  );
-};
 
 export default SubCategories;
--- src/pages/ourservices/subcategories.js
+++ src/pages/ourservices/subcategories.js
@@ -2,7 +2,11 @@ import React from 'react';
 import SubCategories from '../../components/Services/SubCategories';
 
 const subcategories = () => {
-  return <SubCategories />;
+  return (
+    <div style={{ backgroundColor: '#efefef' }}>
+      <SubCategories />
+    </div>
+  );
 };
 
 export default subcategories;
