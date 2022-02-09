2ba1f58ca040d489208126431fc9d9d0e52ef595
AndresLLogares <Andresl940@hotmail.com>
2022-02-09 12:07:08 -0300

    fixed contact

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,11 +12,12 @@ import {
   Button,
   Menu,
   MenuItem,
+  Typography,
 } from '@material-ui/core';
 
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-import OctoLogo from '../assets/logo.svg';
+import HorizontalLogo from '../assets/horizontalLogo.svg';
 import Contact from './Contact';
 import PopUpContact from './PopupContact'; 
 
@@ -153,7 +154,9 @@ const NavLinks = (props) => {
                       to={child.link}
                       onClick={handleClose}
                     >
-                      {child.title}
+                      <Typography className={classes.subItem}>
+                        {child.title}
+                      </Typography>
                     </MenuItem>
                   ))
                 )}
@@ -167,7 +170,7 @@ const NavLinks = (props) => {
           <a
             aria-controls="simple-menu"
             aria-haspopup="true"
-            onClick={() => setShowPopUp(true)}
+            onClick={() => setShowPopUp(!showPopUp)}
             className={classes.link}
             margin={2}
           >
@@ -185,7 +188,11 @@ const NavLinks = (props) => {
           <div className={classes.linkText}>{'BOOK A ZOOM CALL'}</div>
         </Button>
       </Link>
-      <>{showPopUp ? <PopUpContact handleClose={setShowPopUp}/> : null}</>
+      <>
+        {showPopUp ? (
+          <PopUpContact handleClose={setShowPopUp} />
+        ) : null}
+      </>
     </>
   );
 };
@@ -204,17 +211,12 @@ const NavBar = (props) => {
       <HideOnScroll {...props}>
         <AppBar className={classes.appbar}>
           <Toolbar className={classes.toolbar}>
-            <Link to={'/'} className={classes.link}>
+            <Link to={'/'} className={classes.linkLogo}>
               <div className={classes.links}>
-                <div className={classes.logo}>
-                  <OctoLogo className={classes.logo} />
-                </div>
-                <div className={classes.textLogoContainer}>
-                  <p className={classes.textLogo}>{'Octosoft'}</p>
-                  <p className={classes.textLogoSub}>
-                    {'Professionals'}
-                  </p>
-                </div>
+                
+                  
+                  <HorizontalLogo className={classes.logo} />
+                
               </div>
             </Link>
             <Hidden lgUp>
@@ -275,9 +277,17 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'right',
     alignItems: 'center',
   },
+  subItem: {
+    fontFamily: 'Montserrat',
+  },
   link: {
     margin: '15px 10px',
     padding: '0px 15px',
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+    cursor: 'pointer',
+  },
+  linkLogo: {
     color: theme.palette.text.primary,
     textDecoration: 'none',
     cursor: 'pointer',
@@ -308,8 +318,8 @@ const useStyles = makeStyles((theme) => ({
     opacity: ({ transparent }) => (!!transparent ? '0' : '1'),
   },
   logo: {
-    width: 46,
-    height: 46,
+    width: 135,
+    //height: 46,
     marginRight: 10,
     opacity: ({ transparent }) => (!!transparent ? '0' : '1'),
     fill: '#33adff',
